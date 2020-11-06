02c81807f414617a47157ac57055fc6cf25696b7
kilbc634 <tuyn76801@gmail.com>
2020-11-06 21:38:29 +0800

    Make endpoint to sign up. And init Redis client module

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var express = require('express');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var redis = require('redis');
+var md5 = require("md5");
+const setting = require('./setting');
+
+var RedisClient = redis.createClient({
+    host: setting.REDIS_HOST,
+    port: setting.REDIS_PORT,
+    password: setting.REDIS_AUTH
+});
+RedisClient.on('connect', function() {
+    console.log('Redis client connected');
+});
+RedisClient.on('error', function(error) {
+    console.error(error);
+});
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/static', express.static(__dirname + '/static'));
+app.use(session({
+    secret: 'tsukumonotsuki', // change this!
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge : 1000 * 60 * 60 * 12, // unit is 'ms'
+    }
+}));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html'); 
@@ -14,8 +38,8 @@ app.get('/login(.html)?', function (req, res) {
 });
 
 app.post('/login', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
-    res.send(name + ' Submitted Successfully!');
+    // var name = req.body.firstName + ' ' + req.body.lastName;
+    // res.send(name + ' Submitted Successfully!');
 });
 
 app.get('/signup(.html)?', function (req, res) {
@@ -23,11 +47,35 @@ app.get('/signup(.html)?', function (req, res) {
 });
 
 app.post('/signup', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
-    res.send(name + ' Submitted Successfully!');
+    var account = req.body['account'];
+    var key = 'account/' + account;
+    var value = {
+        password: md5(req.body['password']),
+        nick: req.body['nick']
+    }
+    RedisClient.get(key, (error, result) => {
+        console.log(result);
+        if (result === null) {
+            if (error) {console.error(error);}
+            console.log('will sign up for ' + account);
+            RedisClient.set(key, JSON.stringify(value), (error) => {
+                if (error) {console.error(error);}
+                res.json({
+                    status: 'OK',
+                    msg: 'Sign up complated'
+                });
+            })
+        } else {
+                console.log('failed to sign up for ' + account);
+                res.json({
+                    status: 'ERROR',
+                    msg: 'This account is exists'
+                });
+        }
+    });
 });
 
 
-var server = app.listen(5000, function () {
+var server = app.listen(setting.WEB_PORT, function () {
     console.log('Node server is running..');
 });
--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,11 +1,39 @@
 $(function () {
 
+    function post_signup(account, password, nick='') {
+        var obj = {
+            account: account,
+            password: password,
+            nick: nick
+        };
+        var data = JSON.stringify(obj);
+        $.ajax({
+            url: '/signup',
+            type: 'POST',
+            data: data,
+            async: true,
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (resp) {
+                var status = resp['status'];
+                if (status == 'OK') {
+                    location.assign('login.html');
+                } else {
+                    console.error(resp['msg'])
+                }
+            }
+        });
+    }
+
     $('#login-button').click(function () {
-        alert('you click #login-button')
+        // alert('you click #login-button');
     });
 
-    $('#signin-button').click(function () {
-        alert('you click #signin-button')
+    $('#signup-button').click(function () {
+        var account = $('#account-input').val();
+        var password = $('#password-input').val();
+        var name = $('#name-input').val();
+        post_signup(account, password, name);
     });
 
 });
