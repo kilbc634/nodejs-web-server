971af47fce7a27de69f9cf7d17752228b1296bd6
kilbc634 <tuyn76801@gmail.com>
2020-11-18 14:06:14 +0800

    Done for chat room send/load message function - Part 1

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var http = require('http');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var io = require('socket.io')
 var redis = require('redis');
+var moment = require('moment');
+var tz = require('moment-timezone');
+moment.tz.setDefault("Asia/Taipei");
 var md5 = require("md5");
 const setting = require('./setting');
+
+// --------------------- Redis init
 
 var RedisClient = redis.createClient({
     host: setting.REDIS_HOST,
@@ -17,17 +24,24 @@ RedisClient.on('error', function(error) {
     console.error(error);
 });
 
-var app = express();
-app.use(bodyParser.json());
-app.use('/static', express.static(__dirname + '/static'));
-app.use(session({
+// --------------------- Session init
+
+var sessionMiddleware = session({
     secret: 'tsukumonotsuki', // change this!
     resave: true,
     saveUninitialized: false,
     cookie: {
         maxAge : 1000 * 60 * 60 * 12, // unit is 'ms'
     }
-}));
+});
+
+// --------------------- Express init
+
+var app = express();
+var server = http.createServer(app);  // use for socket.io
+app.use(bodyParser.json());
+app.use('/static', express.static(__dirname + '/static'));
+app.use(sessionMiddleware);
 
 app.get('/', function (req, res) {
     res.redirect('/index');
@@ -67,6 +81,7 @@ app.post('/login', function (req, res) {
         }
         req.session.regenerate((err) => {
             req.session.loginUser = account;
+            req.session.room = account;
             res.json({
                 status: 'OK',
                 msg: 'Login success'
@@ -77,7 +92,7 @@ app.post('/login', function (req, res) {
 
 app.get('/logout(.html)?', function (req, res) {
     req.session.destroy((err) => {
-        //req.session.loginUser = null;
+        // req.session.loginUser = null;
     });
     res.redirect('login');
 })
@@ -110,7 +125,75 @@ app.post('/signup', function (req, res) {
     });
 });
 
+app.get('/get_chatMessages', function (req, res) {
+    if (!req.session.loginUser) {
+        res.status(403).send('Permission denied');
+        return;
+    }
+    var begin = parseInt(req.query['begin']) || 0;
+    var amount = parseInt(req.query['amount']) || 20;
+    var key = 'chatQueue/' + req.session.room;
+    RedisClient.lrange(key, begin, begin + amount - 1, (err, result) => {
+        if (result.length > 0) {
+            var msgs = [];
+            for (var string of result) {
+                var msg = JSON.parse(string);
+                msgs.push(msg);
+            }
+            res.json({
+                chatMessages: msgs
+            })
+        } else {
+            res.json({
+                chatMessages: []
+            })
+        }
+    });
+});
 
-var server = app.listen(setting.WEB_PORT, function () {
+// --------------------- Socket.io init
+
+var socketApp = io(server);
+socketApp.use(function (socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});
+
+socketApp.on('connection', (socket) => {
+    var session = socket.request.session;
+    if (session.loginUser) {
+        var loginUser = session.loginUser;
+        var room = session.room;
+        socket.nickname = loginUser;
+        socket.join(room);
+        console.log(`[INFO] User - ${loginUser} enter the room - ${room}`)
+    } else {
+        console.log(`[INFO] Socket connect fail`);
+        return;
+    }
+
+    socket.on('sendMessage', (data) => {
+        var msg = data['msg'];
+        var key = 'chatQueue/' + room;
+        var ts = parseInt(moment().format("X"));
+        var setValue = {
+            timestamp: ts,
+            user: loginUser,
+            text: msg
+        }
+        RedisClient.lpush(key, JSON.stringify(setValue));
+        socketApp.to(room).emit('newMessage', {
+            timestamp: ts,
+            msg: msg
+        });
+    });
+
+    socket.on('test', (data) => {
+        console.log(`[INFO] From ${loginUser}: ${JSON.stringify(data)}`);
+    });
+    
+});
+
+
+var server = server.listen(setting.WEB_PORT, function () {
     console.log('Node server is running..');
 });
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,6 +1,89 @@
-$(document).ready(function () {
+$(function () {
+    moment.tz.setDefault("Asia/Taipei");
+    var socket = io.connect();
+    var begin = 0;
+
+    var msg_card_body = new Vue({
+        el: '.msg_card_body',
+        data: {
+            msgList: []
+        }
+    })
+
+    function loadMessages(begin, amount=20) {
+        var data = {
+            begin: begin,
+            amount: amount
+        }
+        $.ajax({
+            url: '/get_chatMessages',
+            type: 'GET',
+            data: data,
+            async: true,
+            dataType: 'json',
+            success: function (resp) {
+                var dataList = resp['chatMessages'];
+                for (data of dataList) {
+                    msg_card_body.msgList.unshift(
+                        {text: data['text'], date: moment.unix(data['timestamp']).format('YYYY-MM-DD HH:mm:ss')}
+                    )
+                }
+            }
+        });
+        return amount;
+    }
+
+    function sendMessage() {
+        var msg = $('textarea.type_msg').val();
+        if (msg.trim() == '') {
+            $('textarea.type_msg').val(null);
+            return false;
+        }
+        socket.emit('sendMessage', {
+            msg: msg.trim()
+        });
+        $('textarea.type_msg').val(null);
+    }
+
+    $('.send_btn').click(function () {
+        sendMessage();
+    });
+
+    $('textarea.type_msg').keydown(function (event) {
+        if (event.which == 13) {
+            if(event.shiftKey) {
+                // will add new line
+            } else {
+                sendMessage();
+                event.preventDefault();
+            }
+        }
+    });
+
+    function newMessage(data) {
+        var msg = data['msg'];
+        var ts = data['timestamp'];
+        msg_card_body.msgList.push(
+            {text: msg, date: moment.unix(ts).format('YYYY-MM-DD HH:mm:ss')}
+        )
+    }
+
+    socket.on('newMessage', function (data) {
+        newMessage(data);
+        begin += 1;
+    });
+
+    socket.on('test', function (data) {
+        console.log(data);
+    });
+
+    // Init zone
+
     $('#action_menu_btn').click(function () {
         $('.action_menu').toggle();
     });
+    $('.msg_card_body').mCustomScrollbar();
     autosize(document.querySelectorAll('[data-autosize="true"]'));
+    begin += loadMessages(begin);
+
 });
